68c0eb362a7cc1fd303d426b7d11268231f2b2b0
Gustavo Henrique <142763475+TavinHVM@users.noreply.github.com>
2024-10-30 15:56:40 -0300

    Add files via upload

---
+++ scriptinicial.js
@@ -0,0 +1,12 @@
+function cadastrar() {
+    window.location.href = 'cadastro.html';
+}
+
+function registrarVendas() {
+    window.location.href = 'registro.html';
+}
+
+function sair() {
+    alert("Saindo do sistema...");
+    window.location.href = 'index.html';
+}
